55207d462d742f3cd7aa07706733d592942827cf
vijay khatri <vijaykhatri22@gmail.com>
2018-03-09 18:46:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,3 +10,13 @@ function whileLoop(n){
   }
   return "done";
 }
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
+function doWhileLoop(arr){
+  do{
+    arr.pop();
+  }while(arr.length > 0 && maybeTrue());
+  return arr;
+}
