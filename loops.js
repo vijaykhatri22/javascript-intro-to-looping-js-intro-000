f1aff0137f1ce0239b221a37c3a635641516cb3f
vijay khatri <vijaykhatri22@gmail.com>
2018-03-09 18:39:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
 function forLoop(arr){
   for(var i=0 ; i < 25 ; i++){
     arr.push(`I am ${i} strange loop`);
-
   }
   return arr;
+}
+function whileLoop(n){
+  while( n > 0){
+    console.log( n-- );
+  }
+  return "Done";
 }
